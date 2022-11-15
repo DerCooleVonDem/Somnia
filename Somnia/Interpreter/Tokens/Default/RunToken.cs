9bc8e70632d1798a383de8103e68925f5f355ce5
jonas <main_studios@web.de>
2022-11-15 11:24:20 +0100

    Added Variable Support inside Run Token

--- Somnia/Interpreter/Tokens/Default/RunToken.cs
+++ Somnia/Interpreter/Tokens/Default/RunToken.cs
@@ -8,22 +8,43 @@ public class RunToken : Token
 {
     public RunToken() : base("run") { }
 
+    public bool RunFile(string where, int line, string from)
+    {
+        string path = DataUtil.FromRelativePath(from, where);
+        //if file not exists throw a syntax error
+        if (!File.Exists(path))
+        {
+            new SyntaxError().Throw("Invalid file", line, from);
+            return false;
+        }
+
+        Interpreter interpreter = new Interpreter(path);
+        interpreter.Run();
+
+        return true;
+    }
+
     public override bool Run(string body, int line, string where)
     {
         if (DataUtil.IdentifyDataType(body) == STRING)
         {
-            string path = DataUtil.FromRelativePath(where, DataUtil.ToString(body));
-            //if file not exists throw a syntax error
-            if (!File.Exists(path))
+            return RunFile(DataUtil.ToString(body), line, where);
+        }
+
+        if (DataUtil.IdentifyDataType(body) == VARIABLE)
+        {
+            string? unparsed = DataUtil.FromVariable(body);
+            if (unparsed == null)
             {
-                new SyntaxError().Throw("Invalid file", line, where);
-                return false;
+                new SyntaxError().Throw("Invalid variable", line, where);
             }
 
-            Interpreter interpreter = new Interpreter(path);
-            interpreter.Run();
-
-            return true;
+            if (DataUtil.IdentifyDataType(unparsed) != STRING)
+            {
+                new SyntaxError().Throw("Invalid data type for run statement - Expected string/path, got " + DataUtil.DataTypeToString(DataUtil.IdentifyDataType(unparsed)), line, where);
+            }
+            
+            return RunFile(DataUtil.ToString(unparsed), line, where);
         }
         
         new SyntaxError().Throw("Invalid data type for run statement - Expected string/path, got " + DataUtil.DataTypeToString(DataUtil.IdentifyDataType(body)), line, where);
